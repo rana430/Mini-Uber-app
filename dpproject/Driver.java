0db9849dab6c032ae16248d1eaa7a59c0d3240a8
Hager Essam <hajer.192003@gmail.com>
2023-08-31 22:12:39 +0300

    edited

--- dpproject/Driver.java
+++ dpproject/Driver.java
@@ -1,15 +1,18 @@
-package dpproject;
+package TypesOfRides.dpproject;
 
-public class Driver extends APPUser {
+import java.util.Scanner;
+public class Driver extends AppUsers{
+
     private String drivingLicenseNumber;
     private String carLicenseNumber;
-
-    public Driver(String firstName, String lastName, String phoneNumber, String drivingLicenseNumber, String carLicenseNumber, String Password) {
-        super(firstName, lastName, phoneNumber, Password);
+    private Types veichleType;
+    static UsersFile driverFile = UsersFile.getUsersFile();
+    public Driver(String firstName, String lastName, String phoneNumber, String drivingLicenseNumber, String carLicenseNumber, String Password , Types t) {
+        super(firstName , lastName , Password , phoneNumber);
         this.drivingLicenseNumber = drivingLicenseNumber;
         this.carLicenseNumber = carLicenseNumber;
+        this.veichleType = t;
     }
-
     public String getDrivingLicenseNumber() {
         return drivingLicenseNumber;
     }
@@ -17,4 +20,86 @@ public class Driver extends APPUser {
     public String getCarLicenseNumber() {
         return carLicenseNumber;
     }
+
+    public static void createDriver(String role) {
+        Scanner scanner = new Scanner(System.in);
+        Types t = new Types();
+        System.out.println("Enter First name:");
+        String firstName = scanner.next();
+        System.out.println("Enter Last name:");
+        String lastName = scanner.next();
+        System.out.println("Enter Phone Number:");
+        String phoneNumber = scanner.next();
+        System.out.println("Enter Password:");
+        String password = scanner.next();
+        System.out.println("Enter Driving License Number:");
+        String drivingLicenseNumber = scanner.next();
+        System.out.println("Enter Car License Number:");
+        String carLicenseNumber = scanner.next();
+        System.out.println("Enter Car Type: 1)Normal 2)Premium 3)Bus 4)Motorbike");
+        String type = scanner.next();
+
+        driverFile.write(role + "\t" + firstName + "\t" + lastName + "\t" + password + "\t" + phoneNumber + "\t" + drivingLicenseNumber + "\t" + carLicenseNumber + "\t" + type);
+        System.out.println("WELCOME  "+ firstName + lastName);
+
+        Driver build = new DriverBuilder().setFirstName(firstName)
+                .setLastName(lastName)
+                .setPhoneNumber(phoneNumber)
+                .setPassword(password)
+                .setDrivingLicenseNumber(drivingLicenseNumber)
+                .setCarLicenseNumber(carLicenseNumber)
+                .setVeichleType(t, type)
+                .build();
+        build.veichleType.description(10);
+    }
+    public static Driver login(String role) {
+        Scanner scanner = new Scanner(System.in);
+        Types t = new Types();
+        System.out.println("User name :");
+        String UName = scanner.next();
+
+        System.out.println("Password :");
+        String pass = scanner.next();
+
+        String[] driverData =driverFile.read( role , UName , pass);
+        System.out.print("WELCOME  "+ UName);
+        return new DriverBuilder().setFirstName(driverData[1])
+                .setLastName(driverData[2])
+                .setPhoneNumber(driverData[4])
+                .setPassword(driverData[3])
+                .setDrivingLicenseNumber(driverData[5])
+                .setCarLicenseNumber(driverData[6])
+                .setVeichleType(t, driverData[7])
+                .build();
+    }
+    /*switch (type)
+        {
+            case "1":
+                 veichleType.setTypes(new NormalDescprition());
+                 break;
+            case "2":
+                veichleType.setTypes(new PremiumDescription());
+                break;
+            case "3":
+                veichleType.setTypes(new BusDescription());
+                break;
+            case "4":
+                veichleType.setTypes(new MotorbikeDescription());
+                break;
+            default:
+                createDriver("2");
+                break;
+        }
+        driverFile.write(role + "\t" + firstName + "\t" + lastName + "\t" + password + "\t" + phoneNumber + "\t" + drivingLicenseNumber + "\t" + carLicenseNumber + "\t" + type);
+        System.out.print("WELCOME  "+ firstName + lastName);
+
+        Driver build = new DriverBuilder().setFirstName(firstName)
+                .setLastName(lastName)
+                .setPhoneNumber(phoneNumber)
+                .setPassword(password)
+                .setDrivingLicenseNumber(drivingLicenseNumber)
+                .setCarLicenseNumber(carLicenseNumber)
+                .build();
+        veichleType.description(10);
+        return build;*/
 }
--- dpproject/UsersFile.java
+++ dpproject/UsersFile.java
@@ -1,109 +1,45 @@
-package dpproject;
+package TypesOfRides.dpproject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
+import java.io.*;
 public class UsersFile {
-    public static void Register() 
+
+    private UsersFile() {}
+    private static UsersFile UFile = new UsersFile();
+    public static UsersFile getUsersFile ()
+    {
+        return UFile;
+    }
+    public void write(String line)
     {
         try{
-        FileWriter file = new FileWriter("users.txt", true);
-        BufferedWriter UFileWriter = new BufferedWriter(file);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Are You a (Enter the number):\n 1) User \n 2) Driver \n");
-        int choice = scanner.nextInt();
-        System.out.println("Enter First name :");
-        String fName = scanner.next();
-        System.out.println("Enter Last name :");
-        String lName = scanner.next();
-        System.out.println("Enter Phone Number :");
-        String phoneNum = scanner.next();
-        System.out.println("Enter Pasword :");
-        String pass = scanner.next();
-        switch(choice)
-        {
-            case 1:
-                
-                UFileWriter.write("1\t"+fName + "\t" + lName +"\t" + pass +"\t" + phoneNum + "\n");
-                UFileWriter.close();
-                APPUser user = new APPUser(fName , lName , phoneNum , pass);
-                System.out.print("WELCOME  "+ user.getUserName());
-                break;
-            case 2:
-                System.out.println("Enter Driving License Number :");
-                String dLicenseNum = scanner.next();
-                System.out.println("Enter Car License Number :");
-                String cLicenseNum = scanner.next();
-                UFileWriter.write("2\t"+ fName + "\t" + lName +"\t" +pass +"\t" + phoneNum + "\t" + dLicenseNum +"\t" + cLicenseNum +"\n");
-                UFileWriter.close();  
-                Driver driver = new Driver(fName , lName , phoneNum , dLicenseNum , cLicenseNum , pass);
-                System.out.print("WELCOME  "+ driver.getUserName());
-                break;
-        }
+            FileWriter file = new FileWriter("users.txt", true);
+            BufferedWriter UFileWriter = new BufferedWriter(file);
+                    UFileWriter.write(line+ "\n");
+                    UFileWriter.close();
+
         }catch (IOException e) {
             System.out.println("Error writing user data to file.");
-        } 
+        }
     }
-    public static void Login() 
-    {
-        try{
-        FileReader file = new FileReader("users.txt");
-        BufferedReader UFileReader = new BufferedReader(file);
-        Scanner scanner = new Scanner(System.in);
+    public String[] read(String type , String userName , String Pass) {
         String line;
-        boolean found = false;
         String[] userData = {};
-        System.out.println("Are You a (Enter the number):\n 1) User \n 2) Driver \n 3) Admin");
-        int choice = scanner.nextInt();
-        System.out.println("User name :");
-        String uName = scanner.next();
-        
-        System.out.println("Pasword :");
-        String pass = scanner.next();
-        
-        while ((line = UFileReader.readLine()) != null)
-        {
-            userData = line.split("\t");
-            if (userData[0].equals(String.valueOf(choice)))
-            {
-                if ((userData[1]+userData[2]).equals(uName) && userData[3].equals(pass))
-                {
-                    found = true;
-                    break;
+        try {
+            FileReader file = new FileReader("users.txt");
+            BufferedReader UFileReader = new BufferedReader(file);
+            while((line = UFileReader.readLine())!= null) {
+
+                userData = line.split("\t");
+                if ( userData[0].equals(type)) {
+                    if ((userData[1] + userData[2]).equals(userName) && userData[3].equals(Pass)) {
+                        break;
+                    }
                 }
             }
-        }
-        UFileReader.close();
-        if(found == false)
-        {
-             System.out.println("Invalid Data !");
-        }
-        else
-        {
-        switch(choice)
-        {
-            case 1 :
-                APPUser user = new APPUser(userData[1], userData[2], userData[3], userData[4]);
-                System.out.println("WeLCOME User "+ user.getUserName());
-                break;
-            case 2 :
-                Driver driver = new Driver(userData[1], userData[2], userData[3], userData[4],userData[5], userData[6]);
-                System.out.println("WeLCOME Driver "+ driver.getUserName());
-                break;
-            case 3:
-                Admin admin = new Admin();
-                System.out.println("WeLCOME Admin");
-                break;
-        }
-        }
-        }catch (IOException e) {
+            UFileReader.close();
+        } catch (IOException e) {
             System.out.println("Error writing user data to file.");
-        } 
-        
+        }
+        return userData;
     }
 }
-
